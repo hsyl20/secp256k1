c44131985a7a9f1e5e8808066a16a4e2f6e5ebfa
Sylvain Henry <sylvain@haskus.fr>
2025-12-08 17:17:32 +0100

    Make Cabal package

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -euo pipefail
+
+rm -rf test_dir
+mkdir test_dir
+
+cabal sdist
+cd test_dir
+tar xf ../dist-newstyle/sdist/secp256k1-clib-0.7.1.tar.gz
+cd secp256k1-clib-0.7.1
+cabal build -f "+ecdh +ellswift +extrakeys +musig +recovery +schnorrsig"
